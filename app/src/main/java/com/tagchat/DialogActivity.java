19e54c141ef69f3be5bfc1a06ef8d3973fc5c899
evgep <evgepishcin@gmail.com>
2019-06-08 21:39:14 +0300

    dialogActivity add editText and listView

--- app/src/main/java/com/tagchat/DialogActivity.java
+++ app/src/main/java/com/tagchat/DialogActivity.java
@@ -2,12 +2,21 @@ package com.tagchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class DialogActivity extends AppCompatActivity {
 
+    EditText editText;
+    ListView listView;
+    MessageAdapter messageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+        listView = findViewById(R.id.listViewMessages);
+        messageAdapter = new MessageAdapter(this);
+        listView.setAdapter(messageAdapter);
+        editText = findViewById(R.id.editTextMessageText);
     }
 }
